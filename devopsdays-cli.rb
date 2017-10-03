da845798d7e6941f4871271a9fc65c7eff522659
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-03 17:57:57 -0500

    devopsdays-cli version 0.12.8

--- devopsdays-cli.rb
+++ devopsdays-cli.rb
@@ -1,9 +1,9 @@
 class DevopsdaysCli < Formula
   desc "Command-line utilities for the devopsdays website"
   homepage "https://www.devopsdays.org"
-  url "https://github.com/devopsdays/devopsdays-cli/releases/download/0.12.7/devopsdays-cli_0.12.7_darwin-amd64.tar.gz"
-  version "0.12.7"
-  sha256 "d472d61d1757f9b5b0d33519dc0a8698c8f09b8b8bb268e53206377c67887b0d"
+  url "https://github.com/devopsdays/devopsdays-cli/releases/download/0.12.8/devopsdays-cli_0.12.8_darwin-amd64.tar.gz"
+  version "0.12.8"
+  sha256 "ca4852c4a5dce106efd74ecc4e0bca881468234ccacfbd196529133bd8baf4eb"
 
   depends_on "git"
 
