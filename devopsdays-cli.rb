5746df8bef2ee78888e67e75b34a65cd5feb1b7e
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-25 06:59:16 -0500

    Brew formula update for devopsdays-cli version 0.24.1

--- devopsdays-cli.rb
+++ devopsdays-cli.rb
@@ -1,17 +1,13 @@
 class DevopsdaysCli < Formula
   desc "Command-line utilities for the devopsdays website"
   homepage "https://www.devopsdays.org"
-  url "https://github.com/devopsdays/devopsdays-cli/releases/download/0.22.1/devopsdays-cli_0.22.1_darwin-amd64.tar.gz"
-  version "0.22.1"
-  sha256 "dc7077007dd2fedf1055e7114e3d37419160f9f2ad21a8c3b3c51a93ed9bf247"
-
+  url "https://github.com/devopsdays/devopsdays-cli/releases/download/0.24.1/devopsdays-cli_0.24.1_darwin-amd64.tar.gz"
+  version "0.24.1"
+  sha256 "e6b77c8b2985f61f3e6fca30427784addbf09c6de89a61a5eac7962f9e77a297"
+  
   depends_on "git"
 
   def install
     bin.install "devopsdays-cli"
-  end
-
-  test do
-    
   end
 end
